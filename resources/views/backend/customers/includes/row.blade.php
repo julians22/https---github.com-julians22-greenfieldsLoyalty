2a9820ea2f41d1adee2c9d47f12392c870bec89c
julians22 <dabnerjulian@yahoo.com>
2023-06-20 14:17:52 +0700

    update

--- resources/views/backend/customers/includes/row.blade.php
+++ resources/views/backend/customers/includes/row.blade.php
@@ -27,15 +27,15 @@
 </x-livewire-tables::bs4.table.cell>
 
 <x-livewire-tables::bs4.table.cell>
-    {{ $row->detail ? Str::replaceArray('|', ',', $row->detail->history_milk_category) : '' }}
+    {{ $row->detail ? str_replace('|', ',', $row->detail->history_milk_category) : '' }}
 </x-livewire-tables::bs4.table.cell>
 
 <x-livewire-tables::bs4.table.cell>
-    {{ $row->detail ? Str::replaceArray('|', ',', $row->detail->history_milk_product) : '' }}
+    {{ $row->detail ? str_replace('|', ',', $row->detail->history_milk_product) : '' }}
 </x-livewire-tables::bs4.table.cell>
 
 <x-livewire-tables::bs4.table.cell>
-    {{ $row->detail ? Str::replaceArray('|', ',', $row->detail->history_milk_packsize) : '' }}
+    {{ $row->detail ? str_replace('|', ',', $row->detail->history_milk_packsize) : '' }}
 </x-livewire-tables::bs4.table.cell>
 
 <x-livewire-tables::bs4.table.cell>
