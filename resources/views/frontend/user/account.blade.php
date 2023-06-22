5b5738d89789220b9681e7635ac3c8e88a3a2bd0
julians22 <dabnerjulian@yahoo.com>
2023-06-22 17:29:27 +0700

    update

--- resources/views/frontend/user/account.blade.php
+++ resources/views/frontend/user/account.blade.php
@@ -207,7 +207,7 @@
                             <p>Kamu bisa melihat hadiah khusus pengguna baru di sini.</p>
 
                             @if ($logged_in_user->isHasDetail() && $logged_in_user->isHasAddressData() && $logged_in_user->isWhatsappVerified())
-                                @if ($logged_in_user->isWebUser() && $logged_in_user->isWhatsappUser())
+                                @if ($logged_in_user->isWebUser() || $logged_in_user->isWhatsappUser())
                                     <div x-data="{ displayVoucher: false }">
                                         <button class="btn btn-dark-green btn-rounded" type="button" @click="displayVoucher = !displayVoucher">Lihat Hadiah</button>
 
