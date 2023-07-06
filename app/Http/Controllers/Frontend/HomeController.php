3e9253a8ace2c50873af6ff96c884573b862d27d
julians22 <dabnerjulian@yahoo.com>
2023-07-06 16:38:35 +0700

    update

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -40,7 +40,7 @@ class HomeController
                         'user_id' => auth()->user()->id,
                         'reward_id' => $reward->id,
                         'offline_reward' => $reward_offline_id,
-                        'address_id' => auth()->address_data->id,
+                        'address_id' => auth()->user()->address_data->id,
                     ]);
 
                     $reward->current_stock -= 1;
