782b274a06f4067ab2ace2fb5b97ec6d77434028
julians22 <dabnerjulian@yahoo.com>
2023-09-13 13:29:07 +0700

    fix error call method

--- app/Domains/Auth/Models/Traits/Method/UserMethod.php
+++ app/Domains/Auth/Models/Traits/Method/UserMethod.php
@@ -34,7 +34,7 @@ trait UserMethod
             return $this->isHasAddressData();
         }
 
-        if ($this->address_data->isHasCity()) {
+        if ($this->address_data->hasCity()) {
             return false;
         }
 
@@ -45,7 +45,7 @@ trait UserMethod
             return $this->isHasAddressData();
         }
 
-        if ($this->address_data->isHasProvince()) {
+        if ($this->address_data->hasProvince()) {
             return false;
         }
 
@@ -56,7 +56,7 @@ trait UserMethod
             return $this->isHasAddressData();
         }
 
-        if ($this->address_data->isHasDistrict()) {
+        if ($this->address_data->hasDistrict()) {
             return false;
         }
 
